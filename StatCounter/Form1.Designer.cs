4e9105fce2fae959e89f9304f5e6ebc0b0c5b05b
Will Grigg <willwgrigg@gmail.com>
2019-04-30 22:32:29 -0600

    first commit for v1

---
+++ StatCounter/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace StatCounter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.tbFileName = new System.Windows.Forms.TextBox();
+            this.lblStats = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(18, 62);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(86, 41);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open File";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // tbFileName
+            // 
+            this.tbFileName.Location = new System.Drawing.Point(18, 34);
+            this.tbFileName.Name = "tbFileName";
+            this.tbFileName.ReadOnly = true;
+            this.tbFileName.Size = new System.Drawing.Size(681, 22);
+            this.tbFileName.TabIndex = 1;
+            // 
+            // lblStats
+            // 
+            this.lblStats.AutoSize = true;
+            this.lblStats.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStats.Location = new System.Drawing.Point(12, 141);
+            this.lblStats.Name = "lblStats";
+            this.lblStats.Size = new System.Drawing.Size(0, 32);
+            this.lblStats.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 450);
+            this.Controls.Add(this.lblStats);
+            this.Controls.Add(this.tbFileName);
+            this.Controls.Add(this.btnOpen);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Spreadsheet Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox tbFileName;
+        private System.Windows.Forms.Label lblStats;
+    }
+}
+
---
+++ StatCounter/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace StatCounter
+{
+
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                lblStats.Text = "";
+
+                tbFileName.Text = ofd.FileName;
+                Excel excel = new Excel(ofd.FileName, 1);
+
+                double solids = 0;
+                double cutstock = 0;
+                double fj = 0;
+                double core = 0;
+                double backrip = 0;
+                double waste = 0;
+                double percentTotal = 0;
+
+                int backrip_quantity = 0;
+
+                lblStats.Text = "LOADING...";
+
+                int volPercentX = 0; //volume % x location
+                int quantityX = 0;   //piece count x location
+                int minLengthX = 0;  //min length x location
+                int nameX = 0;       //name x location
+
+                // this will run until they are all non-zero values
+                for (int i = 5; (volPercentX * quantityX * minLengthX * nameX) == 0; i++)
+                {
+                    string name = excel.ReadCell(2, i);
+
+                    if (name == "Name")
+                    {
+                        nameX = i;
+                    }
+                    else if (name == "Min. Length")
+                    {
+                        minLengthX = i;
+                    }
+                    else if (name == "Quantity\n[ pcs ] ")
+                    {
+                        quantityX = i;
+                    }
+                    else if (name == "Volume\n[ % ] ")
+                    {
+                        volPercentX = i;
+                    }
+                }
+                for (int i = 3; excel.ReadCell(i, 8) != ""; i++)
+                {
+                    double min_length = Double.Parse(excel.ReadCell(i, minLengthX));
+
+                    double volume;
+                    volume = Double.Parse(excel.ReadCell(i, volPercentX));
+
+                    if (min_length < 8)
+                    {
+                        string name = excel.ReadCell(i, nameX);
+                        if (name.Contains("Core"))
+                        {
+                            core += volume;
+                        }
+                        else if (name.Contains("Backrip"))
+                        {
+                            backrip += volume;
+                            backrip_quantity += int.Parse(excel.ReadCell(i, quantityX));
+                        }
+                        else
+                        {
+                            fj += volume;
+                        }
+                    }
+                    else if (min_length <= 69 && min_length > 7)
+                    {
+                        cutstock += volume;
+                    }
+                    else if (min_length <= 192 && min_length >= 69.5)
+                    {
+                        solids += volume;
+                    }
+                }
+
+                waste = Double.Parse(excel.ReadCell(46, 3)); // this is the waste%
+
+                percentTotal = solids + cutstock + fj + core + backrip + waste;
+
+                lblStats.Text = "Waste: " + waste.ToString() + "%" + "\nSolids: " + solids.ToString() + "%" +
+                    "\nCutstock: " + cutstock.ToString() + "%" +
+                     "\nFJ: " + fj.ToString() + "%" + "\nCore: " + core.ToString() + "%" + "\nBackrip: " + backrip.ToString() + "%" +
+                    "\nBackrip piece count: " + backrip_quantity.ToString() + " pcs" + "\n\nTOTAL: " + percentTotal.ToString() + "%";
+
+                excel.Close();
+            }
+        }
+    }
+    class Excel
+    {
+        string path = "";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+
+        public Excel(string path, int sheet)
+        { 
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[sheet];
+        }
+
+        public string ReadCell(int i, int j)
+        {
+            var value = ws.Cells[i, j].Value2;
+            if (value != null)
+            {
+                return value.ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public void Close()
+        {
+            this.excel.ActiveWorkbook.Close(0);
+            this.excel.Quit();
+        }
+    }
+}
